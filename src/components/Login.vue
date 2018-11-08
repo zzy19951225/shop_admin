4836d21c4b4bdfe63c3a6c83b916b522787fe62e
zzy19951225 <512381961@qq.com>
2018-11-08 16:53:02 +0800

    登录组件的静态结构

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,63 @@
 <template>
   <div class="login">
-    登录组件
+    <el-form ref="form" :model="form" label-width="80px">
+      <img src="../assets/avatar.jpg" alt="">
+  <el-form-item label="用户名">
+    <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+  </el-form-item>
+   <el-form-item label="密码">
+    <el-input v-model="form.password" placeholder="请输入密码"></el-input>
+  </el-form-item>
+
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
+    <el-button>重置</el-button>
+  </el-form-item>
+</el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less">
+.login {
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .el-form {
+    width: 400px;
+    margin: 200px auto;
+    background-color: #fff;
+    padding: 75px 40px 15px;
+    border-radius: 20px;
+    position: relative;
+  }
+  img {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: -70px;
+    border-radius: 50%;
+    border: 10px solid #fff;
+  }
+  .el-button + .el-button {
+    margin-left: 100px;
+  }
+}
 </style>
