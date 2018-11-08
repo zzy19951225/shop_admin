13eab74ee991beb2f4f02632b5037be6d5731273
zzy19951225 <512381961@qq.com>
2018-11-08 19:06:34 +0800

    登录组件完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,68 @@
 <template>
   <div class="login">
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px" status-icon>
       <img src="../assets/avatar.jpg" alt="">
-  <el-form-item label="用户名">
-    <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+  <el-form-item label="用户名" prop="username">
+    <el-input v-model="form.username" placeholder="请输入用户名" ></el-input>
   </el-form-item>
-   <el-form-item label="密码">
-    <el-input v-model="form.password" placeholder="请输入密码"></el-input>
+   <el-form-item label="密码"  prop="password" >
+    <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
   </el-form-item>
 
   <el-form-item>
-    <el-button type="primary" @click="onSubmit">登录</el-button>
-    <el-button>重置</el-button>
+    <el-button type="primary" @click="login">登录</el-button>
+    <el-button @click="reset">重置</el-button>
   </el-form-item>
 </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit() {
-      console.log('submit!')
+    // 表单重置
+    reset() {
+      this.$refs.form.resetFields()
+    },
+    // 登录
+    login() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          axios({
+            url: 'http://localhost:8888/api/private/v1/login',
+            method: 'post',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              this.$message.success('登录成功')
+            } else {
+              this.$message.error(res.data.meta.msg)
+            }
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
 
 Vue.use(Router)
 export default new Router({
@@ -12,6 +13,10 @@ export default new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
